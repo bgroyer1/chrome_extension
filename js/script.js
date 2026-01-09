0c84a7bfdf1ef4ee4ad6313c477dd4f7263abac6
Braedan Royer <royerbraedan@gmail.com>
2026-01-09 15:53:25 -0800

    added an error span to alert user when trying to add items that already have been added, or empty list items

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
+/* Global Variables */
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
+const ulEl = document.querySelector("#ul-el");
+const errorSpan = document.querySelector("#error-span");
 const myLeads = [];
 
+/* Functions */
 function saveLead() {
   if (!myLeads.includes(inputEl.value.toLowerCase()) && inputEl.value) {
-    myLeads.push(inputEl.value)
-    
+    errorSpan.textContent = "";
+    myLeads.push(inputEl.value);
+  }else if (!inputEl.value) {
+    errorSpan.textContent = "Empty Input";
+  } else {
+    errorSpan.textContent = `Item already added`;
   }
-  inputEl.value = ''
-  console.log(myLeads)
-  console.log("button clicked!");
 }
 
-inputBtn.addEventListener("click", saveLead);
+function renderLeadsHTML() {
+  let listHTML = "";
+  myLeads.forEach((lead) => {
+    listHTML += `<li class='list-item'>${lead}</li>`;
+  });
+  return listHTML;
+}
+
+function renderList() {
+  saveLead();
+  const listHTML = renderLeadsHTML();
+  ulEl.innerHTML = listHTML;
+  inputEl.value = ''
+}
+
+/* Event Listeners */
+inputBtn.addEventListener("click", renderList);

1613ef4798235fd15d4d53616893619823baddcb
Braedan Royer <royerbraedan@gmail.com>
2026-01-09 15:03:37 -0800

    added a myLeads array global variable, created a variable containing the input element

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const inputBtn = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el");
+const myleads = [];
+
+function saveLead() {
+  console.log("button clicked!");
+}
+
+inputBtn.addEventListener("click", saveLead);
